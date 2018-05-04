35ac346a8016fd5b70a3907fd85c5e0c6e59b1da
adolfo angel de la Cruz Díaz <adolfo_a1513@hotmail.com>
2018-05-04 16:39:58 -0500

    Se agrega la pueba de la función eliminarActividad

--- test/mx/fei/dao/ActividadDAOTest.java
+++ test/mx/fei/dao/ActividadDAOTest.java
@@ -8,36 +8,95 @@
 */
 package mx.fei.dao;
 
+import java.util.ArrayList;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import mx.fei.dominio.Actividad;
 import java.util.List;
+import mx.fei.dominio.GeneradorClave;
 
 public class ActividadDAOTest {
     
-    ActividadDAO actividadDAO = new ActividadDAO();
+    private List<Actividad> listaActividades = new ArrayList<>();
+    private ActividadDAO actividadDAO = new ActividadDAO();
+    private int primerId = GeneradorClave.obtenerId();
+    private int segundoId = GeneradorClave.obtenerId();
     
     public ActividadDAOTest() {
+        Actividad actividad1 = new Actividad();
+        actividad1.setId(primerId);
+        actividad1.setNombre("Smell Code");
+        actividad1.setFecha("28/03/2018");
+        actividad1.setFormaDeOperar("Reporte de lectura del texto Smell Code");
+        
+        Actividad actividad2 = new Actividad();
+        actividad2.setId(segundoId);
+        actividad2.setNombre("Secure Code");
+        actividad2.setFecha("20/03/2018");
+        actividad2.setFormaDeOperar("Reporte de lectura del texto Secure Code");
+        
+        listaActividades.add(actividad1);
+        listaActividades.add(actividad2);
+    }
+    
+    /*
+    *   Funciones Test del método agregar contacto.
+    */
+    @Test
+    public void pruebaAgregarContacto(){
+        
+        boolean success = false;
+        
+        for(int i = 0; i < listaActividades.size(); i++){
+            actividadDAO.agregarActividad(listaActividades.get(i), 123456);
+        }
+        
+        List<Actividad> actividades = actividadDAO.obtenerActividades(123456);
+        
+        for(int j = 0; j < actividades.size(); j++){
+            if(actividades.get(j).getId() == primerId){
+                success = true;
+            }
+        }
+        
+        if(success == true){
+            success = false;
+            for(int j = 0; j < actividades.size(); j++){
+                if(actividades.get(j).getId() == segundoId){
+                    success = true;
+                }
+            }
+        }
+        
+        assertEquals("Verifica si actividad ha sido agregada correctamente", true, success);
     }
     
     //Funiones Test del método obtenerActividades de la clase Actividad
     @Test
     public void pruebaCantidadActividades(){
-        int numeroActividades = 2;
-        
         List<Actividad> actividades = actividadDAO.obtenerActividades(123456);
         
-        assertEquals("Catidad de Actividades en la BD", numeroActividades, actividades.size());
+        assertEquals("Cantidad de Actividades en la BD", 6, actividades.size());
     }
     
     @Test
-    public void pruebaInformacionActividades(){
-        int idEsperado = 12345;
+    public void pruebaEliminarActividad(){
+        
+        boolean success = true;
         
         List<Actividad> actividades = actividadDAO.obtenerActividades(123456);
+        int idEliminado = actividades.get(0).getId();
+        actividadDAO.eliminarActividad(idEliminado);
         
-        int idObtenido = actividades.get(0).getId();
+        actividades = actividadDAO.obtenerActividades(123456);
         
-        assertEquals("Verifica ID de actividades", idEsperado, idObtenido);
+        for(int i = 0; i < actividades.size(); i++){
+            if(actividades.get(i).getId() == idEliminado){
+                success = false;
+            }
+        }
+        
+        assertEquals("Verifica si la actividad fue eliminada", true, success);
     }
+    
 }
