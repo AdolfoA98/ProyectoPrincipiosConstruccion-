f812c7c624df0ffd4d3f8af867f3a62dc9e34e57
adolfo angel de la Cruz Díaz <adolfo_a1513@hotmail.com>
2018-05-13 18:10:54 -0500

    Cambios menores a la clase ActividadDAO y la inclusión de la función editarActividad en la misma

--- src/mx/fei/dao/ActividadDAO.java
+++ src/mx/fei/dao/ActividadDAO.java
@@ -8,13 +8,11 @@
 */
 package mx.fei.dao;
 
-import java.util.ArrayList;
-import java.security.SecureRandom;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -48,8 +46,11 @@ public class ActividadDAO implements IActividadDAO{
                 actividad.setFormaDeOperar(tablaSQL.getString("forma_operar"));
                 listaActividades.add(actividad);
             }
+            conexion.close();
         }catch(SQLException ex){
             Logger.getLogger(ActividadDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            DataBase.cerrarConexion();
         }
         
         return listaActividades;
@@ -77,6 +78,8 @@ public class ActividadDAO implements IActividadDAO{
             success = comando.execute();
         }catch(SQLException ex){
             Logger.getLogger(ActividadDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            DataBase.cerrarConexion();
         }
         
         return success;
@@ -95,9 +98,64 @@ public class ActividadDAO implements IActividadDAO{
             success = comando.execute();
         }catch(SQLException ex){
             Logger.getLogger(ActividadDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }finally{
+            DataBase.cerrarConexion();
         }
         
         return success;
     }
+
+    @Override
+    public boolean editarActividad(Actividad actividadActualizada, int idObjetivo) {
+        
+        String informacionActividad[] = new String[4];
+        informacionActividad[0] = Integer.toString(idObjetivo);
+        informacionActividad[1] = actividadActualizada.getNombre();
+        informacionActividad[2] = actividadActualizada.getFecha();
+        informacionActividad[3] = actividadActualizada.getFormaDeOperar();
+        
+        boolean success = true;
+        conexion = DataBase.obtenerConexion();
+        
+        try{
+            for(int i = 0; i < 4; i++){
+                query = getColumnaQuery(i);
+                PreparedStatement comando = conexion.prepareStatement(query);
+                if(i == 0){
+                    comando.setInt(1, Integer.parseInt(informacionActividad[i]));
+                }else{
+                    comando.setString(1, informacionActividad[i]);
+                }
+                
+                comando.setInt(2, actividadActualizada.getId());
+                comando.execute();
+            }
+        }catch(SQLException ex){
+            Logger.getLogger(ActividadDAO.class.getName()).log(Level.SEVERE, null, ex);
+            success = false;
+        }finally{
+            DataBase.cerrarConexion();
+        }
+        
+        return success;
+    }
+    
+    private String getColumnaQuery(int columna){
+        
+        String query = "";
+        
+        switch(columna){
+            case 0: query = "update actividad set idObjetivo = ? where idActividad = ?";
+            break;
+            case 1: query = "update actividad set nombre = ? where idActividad = ?";
+            break;
+            case 2: query = "update actividad set fecha = ? where idActividad = ?";
+            break;
+            case 3: query = "update actividad set forma_operar = ? where idActividad = ?";
+            break;
+        }
+        
+        return query;
+    } 
 
 }
