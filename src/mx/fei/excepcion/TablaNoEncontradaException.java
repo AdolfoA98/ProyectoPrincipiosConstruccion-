c7b0878186279b45019fac2348a99ae6bf9ecb57
adolfo angel de la Cruz Díaz <adolfo_a1513@hotmail.com>
2018-05-13 18:31:21 -0500

    Se agrega la excepcion TablaNoEncontradaException

---
+++ src/mx/fei/excepcion/TablaNoEncontradaException.java
@@ -0,0 +1,9 @@
+
+package mx.fei.excepcion;
+
+public class TablaNoEncontradaException extends Exception{
+    
+    public TablaNoEncontradaException(String mensaje){
+        super(mensaje);
+    }
+}
