913cb39d815eb70deb63d0e1a0d39a11e5af1082
adolfo angel de la Cruz Díaz <adolfo_a1513@hotmail.com>
2018-06-10 00:15:46 -0500

    Se modifica la pagina inicializadora para abrir la pagina principal del sistema

--- src/sgpa/SGPA.java
+++ src/sgpa/SGPA.java
@@ -5,19 +5,29 @@
  */
 package sgpa;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import mx.fei.dao.ActividadDAO;
+import mx.fei.dominio.Actividad;
 
-public class SGPA
-{
+public class SGPA extends Application{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
-    {
-        ActividadDAO actividadDao = new ActividadDAO();
+    public static void main(String[] args){
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/mx/fei/gui/Principal.fxml"));
         
-        actividadDao.obtenerActividades(123456);
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
